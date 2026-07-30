ef9a9a128d72103a1411628de84ea3cb2ca0fa6e
Mateus Santos <mateuespele2015@gmail.com>
2026-07-30 09:49:26 -0300

    chore: update boot

--- src/components/splash.tsx
+++ src/components/splash.tsx
@@ -2,6 +2,10 @@ import { useAuth } from '@/contexts/auth-provider';
 import { SplashScreen } from 'expo-router';
 
 SplashScreen.preventAutoHideAsync();
+SplashScreen.setOptions({
+  duration: 3000,
+  fade: true,
+});
 
 export function SplashScreenController() {
   const { isLoading } = useAuth();
